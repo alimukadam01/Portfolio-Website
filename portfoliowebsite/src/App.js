d81000500c5fd50796cb3d075862954a668306ff
Ali Murtaza Mukadam <amurtaza907@gmail.com>
2022-09-01 03:24:02 +0500

    31/08/22

--- portfoliowebsite/src/App.js
+++ portfoliowebsite/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import MainSection from './components/MainSection';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <MainSection/>
       <h1>Hello, Mr.Mukadam :)</h1>
       
     </div>
--- portfoliowebsite/src/components/Navbar.js
+++ portfoliowebsite/src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Navbar.css';
+
 
 function Navbar() {
   return (
