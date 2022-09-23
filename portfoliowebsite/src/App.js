1d519139ff1ba942f2e4efe05a9329cf3762dbab
Ali Murtaza Mukadam <amurtaza907@gmail.com>
2022-09-24 00:17:01 +0500

    added footer component

--- portfoliowebsite/src/App.js
+++ portfoliowebsite/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import MainSection from './components/MainSection';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <MainSection/>
+      <Footer/>    
       <h1>Hello, Mr.Mukadam :)</h1>
       
     </div>
---
+++ portfoliowebsite/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+
+function Footer() {
+  return (
+    <>
+        <div className='footer-container'>
+            <h5><a href='/'>Get in touch</a></h5>
+            <h5><a href='/'>Services</a></h5>
+        </div>
+    </> 
+  )
+}
+
+export default Footer;
