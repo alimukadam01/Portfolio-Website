1342de307b3624c024c0f2422880343459e42e1a
Ali Murtaza Mukadam <amurtaza907@gmail.com>
2022-09-27 19:10:14 +0500

    27/9/22

--- portfoliowebsite/src/components/MainSection.js
+++ portfoliowebsite/src/components/MainSection.js
@@ -6,23 +6,24 @@ function MainSection() {
         <>
 
                 <div className="MainSection">
-                    <img src="images/3.png" alt="/" />
-                    <div className="main-caption">
-                        <h2>Hi, I'm </h2><h1>Ali Mukadam<br/></h1><h6>Software Engineer. Graphic
+                    <div className='mainsec-conatiner'>
+                        <div className='software-btn'>
+                            <button className='btn btn-outline-warning'>
+                                <h6>Explore my Software side</h6>
+                            </button>    
+                        </div>
+                        <div className='main-caption'>
+                            <h2>Hi, I'm </h2><h1>Ali Mukadam<br/></h1><h6>Software Engineer. Graphic
                             Desginer.<br/></h6><h2> It's nice to have you here!</h2>
-                        <button className='btn btn-light'>
-                            Get Started
-                        </button>
-                    </div>
-                    <div className='Software'>
-                        <button className='btn btn-outline-warning'>
-                            <h6>Explore my Software side</h6>
-                        </button>
-                    </div>
-                    <div className='Graphics'>
-                        <button className='btn btn-outline-warning'>
-                            <h6>Explore my Graphics side</h6>
-                        </button>
+                            <button className='btn btn-light'>
+                                Get Started
+                            </button>
+                        </div>
+                        <div className='graphics-btn'>
+                            <button className='btn btn-outline-warning'>
+                                <h6>Explore my Graphics side</h6>
+                            </button>
+                        </div>
                     </div>
                 </div>
         </>
